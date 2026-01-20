ac0a520e62275fe691d341709637be65495672ca
mdharis090 <harisnazeer090@gmail.com>
2026-01-20 18:06:31 +0500

    A descriptive message of your changes

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,4 +1,190 @@
-
+//
+// import 'package:flutter/material.dart';
+//
+// import 'bookmarkbloc/bookmark_screen.dart';
+// import 'surah_list_screen.dart';
+// import 'juz_list_screen.dart';
+//
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.green.shade50,
+//       appBar: AppBar(
+//         title: const Text('Quran Pak'),
+//         centerTitle: true,
+//         backgroundColor: Colors.green.shade700,
+//         foregroundColor: Colors.white,
+//         elevation: 6,
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16),
+//         child: Column(
+//           children: [
+//             const SizedBox(height: 20),
+//
+//             Container(
+//               width: double.infinity,
+//               padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+//               decoration: BoxDecoration(
+//                 gradient: LinearGradient(
+//                   colors: [Colors.green.shade400, Colors.green.shade200],
+//                   begin: Alignment.topLeft,
+//                   end: Alignment.bottomRight,
+//                 ),
+//                 borderRadius: BorderRadius.circular(20),
+//                 boxShadow: const [
+//                   BoxShadow(
+//                     color: Colors.black26,
+//                     blurRadius: 8,
+//                     offset: Offset(0, 4),
+//                   ),
+//                 ],
+//               ),
+//               child: const Column(
+//                 children: [
+//                   Text(
+//                     '﷽',
+//                     style: TextStyle(fontSize: 36),
+//                   ),
+//                   SizedBox(height: 12),
+//                   Text(
+//                     'Read Quran Daily',
+//                     style: TextStyle(
+//                       fontSize: 20,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                   SizedBox(height: 6),
+//                   Text(
+//                     'Keep your heart connected with the Quran every day',
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                       fontSize: 14,
+//                       color: Colors.white70,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//
+//             const SizedBox(height: 40),
+//
+//             // 🔘 MENU GRID (Column-wise)
+//             Expanded(
+//               child: GridView.count(
+//                 crossAxisCount: 2, // 2 columns
+//                 mainAxisSpacing: 16,
+//                 crossAxisSpacing: 16,
+//                 children: [
+//                   _menuCard(
+//                     image: 'Surah.png',
+//                     icon: Icons.play_circle_fill,
+//                     title: 'Resume',
+//                     color: Colors.orange.shade400,
+//                     onTap: () {
+//                       Navigator.push(context, MaterialPageRoute(builder: (_)=> BookmarkScreen())
+//                       );
+//                     },
+//                   ),
+//                   _menuCard(
+//                     image: 'Surah.png',
+//                     icon: Icons.menu_book,
+//                     title: 'Juz Index',
+//                     color: Colors.green.shade400,
+//                     onTap: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                           builder: (_) => const JuzListScreen(),
+//                         ),
+//                       );
+//                     },
+//                   ),
+//                   _menuCard(
+//                     image: 'Surah.png',
+//                     icon: Icons.library_books,
+//                     title: 'Surah Index',
+//                     color: Colors.blue.shade400,
+//                     onTap: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                           builder: (_) => const SurahListScreen(),
+//                         ),
+//                       );
+//                     },
+//                   ),
+//                   _menuCard(
+//                     image: 'Surah.png',
+//                     icon: Icons.bookmark,
+//                     title: 'Bookmarks',
+//                     color: Colors.purple.shade400,
+//                     onTap: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(builder: (_) => const BookmarkScreen()),
+//                       );
+//                     },
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _menuCard({
+//     required IconData icon,
+//     required String image,
+//     required String title,
+//     required VoidCallback onTap,
+//     required Color color,
+//   }) {
+//     return GestureDetector(
+//       onTap: onTap,
+//       child: Container(
+//         padding: const EdgeInsets.symmetric(vertical: 20),
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(
+//             colors: [color.withOpacity(0.9), color.withOpacity(0.7)],
+//             begin: Alignment.topLeft,
+//             end: Alignment.bottomRight,
+//           ),
+//           borderRadius: BorderRadius.circular(16),
+//           boxShadow: const [
+//             BoxShadow(
+//               color: Colors.black26,
+//               blurRadius: 6,
+//               offset: Offset(0, 3),
+//             ),
+//           ],
+//         ),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Icon(icon, size: 40, color: Colors.white),
+//             const SizedBox(height: 12),
+//             Text(
+//               title,
+//               textAlign: TextAlign.center,
+//               style: const TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.white,
+//                 fontSize: 16,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 import 'bookmarkbloc/bookmark_screen.dart';
@@ -25,31 +211,19 @@ class HomeScreen extends StatelessWidget {
           children: [
             const SizedBox(height: 20),
 
-            // 🌙 HEADER CARD
+            /// 🔰 HEADER
             Container(
               width: double.infinity,
               padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
               decoration: BoxDecoration(
                 gradient: LinearGradient(
                   colors: [Colors.green.shade400, Colors.green.shade200],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
                 ),
                 borderRadius: BorderRadius.circular(20),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 8,
-                    offset: Offset(0, 4),
-                  ),
-                ],
               ),
               child: const Column(
                 children: [
-                  Text(
-                    '﷽',
-                    style: TextStyle(fontSize: 36),
-                  ),
+                  Text('﷽', style: TextStyle(fontSize: 36)),
                   SizedBox(height: 12),
                   Text(
                     'Read Quran Daily',
@@ -59,44 +233,36 @@ class HomeScreen extends StatelessWidget {
                       color: Colors.white,
                     ),
                   ),
-                  SizedBox(height: 6),
-                  Text(
-                    'Keep your heart connected with the Quran every day',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.white70,
-                    ),
-                  ),
                 ],
               ),
             ),
 
             const SizedBox(height: 40),
 
-            // 🔘 MENU GRID (Column-wise)
+
             Expanded(
               child: GridView.count(
-                crossAxisCount: 2, // 2 columns
+                crossAxisCount: 2,
                 mainAxisSpacing: 16,
                 crossAxisSpacing: 16,
                 children: [
                   _menuCard(
-                    icon: Icons.play_circle_fill,
+                    imagePath: 'assets/resum.png',
                     title: 'Resume',
-                    color: Colors.orange.shade400,
+                    color: Colors.orange,
                     onTap: () {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Resume feature coming soon'),
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const BookmarkScreen(),
                         ),
                       );
                     },
                   ),
                   _menuCard(
-                    icon: Icons.menu_book,
+                    imagePath: 'assets/juz.png',
                     title: 'Juz Index',
-                    color: Colors.green.shade400,
+                    color: Colors.green,
                     onTap: () {
                       Navigator.push(
                         context,
@@ -107,9 +273,9 @@ class HomeScreen extends StatelessWidget {
                     },
                   ),
                   _menuCard(
-                    icon: Icons.library_books,
+                    imagePath: 'assets/Surah.png',
                     title: 'Surah Index',
-                    color: Colors.blue.shade400,
+                    color: Colors.blue,
                     onTap: () {
                       Navigator.push(
                         context,
@@ -120,13 +286,15 @@ class HomeScreen extends StatelessWidget {
                     },
                   ),
                   _menuCard(
-                    icon: Icons.bookmark,
+                    imagePath: 'assets/book.png',
                     title: 'Bookmarks',
-                    color: Colors.purple.shade400,
+                    color: Colors.purple,
                     onTap: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (_) => const BookmarkScreen()),
+                        MaterialPageRoute(
+                          builder: (_) => const BookmarkScreen(),
+                        ),
                       );
                     },
                   ),
@@ -140,7 +308,7 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _menuCard({
-    required IconData icon,
+    required String imagePath,
     required String title,
     required VoidCallback onTap,
     required Color color,
@@ -148,12 +316,9 @@ class HomeScreen extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20),
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [color.withOpacity(0.9), color.withOpacity(0.7)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
           ),
           borderRadius: BorderRadius.circular(16),
           boxShadow: const [
@@ -167,15 +332,32 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(icon, size: 40, color: Colors.white),
+            Container(
+              height: 85,
+              width: 85,
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                shape: BoxShape.circle,
+              ),
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.contain,
+              ),
+            ),
+
+            //   Image.asset(
+          //     imagePath,
+          //     height: 48,
+          //     width: 48,
+          //     color: Colors.white,
+          //   ),
             const SizedBox(height: 12),
             Text(
               title,
-              textAlign: TextAlign.center,
               style: const TextStyle(
-                fontWeight: FontWeight.bold,
                 color: Colors.white,
-                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ],
